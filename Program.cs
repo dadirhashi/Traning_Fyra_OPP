9010bbc8fa0da52a824bd439733df8e4696d03c6
Abdulkadir Hashi <dadirhashi544@gmail.com>
2025-09-21 14:52:03 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Traning_Fyra_OPP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+        }
+
+
+        public class person
+        {
+            public string Name;
+            public int Age;
+
+            public void Introduce()
+            {
+                Console.WriteLine($"Hello, my name is {Name} and I am {Age} years old.");
+            }
+
+
+
+
+        }
+    }
+}
